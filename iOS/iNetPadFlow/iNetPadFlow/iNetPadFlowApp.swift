53a623d5f14435edbb772efd2415dd8d73eabcc0
Robert Sale <robertmsale@icloud.com>
2023-12-22 16:56:20 -0800

    Creation of iOS project

---
+++ iOS/iNetPadFlow/iNetPadFlow/iNetPadFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNetPadFlowApp.swift
+//  iNetPadFlow
+//
+//  Created by Robert Sale on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iNetPadFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
